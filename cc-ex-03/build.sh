a94bf3cd254c816eccf7f53b3ecc2d8b6b552df7
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-07-10 18:27:49 +0200

    add frontend

--- cc-ex-03/build.sh
+++ cc-ex-03/build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 docker rm -f backend
+docker rm -f frontend
 
-docker build -t "backend" Backend
-docker build -t "frontend" Frontend
+CC_BS="server 127.0.0.1:8080"
+docker build -t "mrow4a/backend" Backend
 
-docker run --name backend -d -p 8080:80 backend:latest
-docker run --name frontend -d -p 8080:80 frontend:latest
+#docker build --build-arg CC_BACKEND_SERVERS=${CC_BS} -t "mrow4a/frontend" Frontend
+docker build --build-arg CC_BACKEND_SERVERS="server 127.0.0.1:8000 backup;" -t "mrow4a/frontend" Frontend
+
+docker run --name backend -d -p 8000:80 mrow4a/backend:latest
+docker run --name frontend -d -p 8080:80 mrow4a/frontend:latest
 
 docker ps -a
