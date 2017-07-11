01957df101932ca563814c3ed44dc3852715d10c
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-07-11 16:50:23 +0200

    Fix docker file

--- cc-ex-03/build.sh
+++ cc-ex-03/build.sh
@@ -3,13 +3,12 @@
 docker rm -f backend
 docker rm -f frontend
 
-CC_BS="server 127.0.0.1:8080"
+CC_BS="172.20.0.1"
 docker build -t "mrow4a/backend" Backend
+docker run --name backend -d -p 8000:80 mrow4a/backend:latest
 
 #docker build --build-arg CC_BACKEND_SERVERS=${CC_BS} -t "mrow4a/frontend" Frontend
-docker build --build-arg CC_BACKEND_SERVERS="server 127.0.0.1:8000 backup;" -t "mrow4a/frontend" Frontend
-
-docker run --name backend -d -p 8000:80 mrow4a/backend:latest
+docker build --build-arg CC_BACKEND_SERVERS=$CC_BS -t "mrow4a/frontend" Frontend
 docker run --name frontend -d -p 8080:80 mrow4a/frontend:latest
 
 docker ps -a
