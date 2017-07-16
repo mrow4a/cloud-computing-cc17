6d5ec0f2233722e022466234f38780e9b520f3b7
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-07-16 15:22:39 +0200

    fixes

--- cc-ex-03/build.sh
+++ cc-ex-03/build.sh
@@ -3,20 +3,21 @@
 docker rm -f backend
 docker rm -f frontend
 
-export CC_BACKEND_SERVERS="172.20.0.1"
+export CC_BACKEND_SERVERS="172.17.0.1"
 docker build -t "mrow4a/backend" Backend
 docker run --name backend -d -p 8000:80 mrow4a/backend:latest
 
 #docker build --build-arg CC_BACKEND_SERVERS=${CC_BS} -t "mrow4a/frontend" Frontend
 docker build -t "mrow4a/frontend" Frontend
 #docker run --name frontend --env CC_BACKEND_SERVERS=${CC_BACKEND_SERVERS} -v /etc:/hypervisor_etc -d -p 8080:80 mrow4a/frontend:latest
-docker run --name frontend --env CC_BACKEND_SERVERS=$CC_BACKEND_SERVERS -v /etc:/hypervisor_etc -p 8080:80 mrow4a/frontend:latest
+docker run --name frontend --env CC_BACKEND_SERVERS=$CC_BACKEND_SERVERS -v /etc:/hypervisor_etc -d -p 8080:80 mrow4a/frontend:latest
 docker ps -a
 curl localhost:8080
 curl localhost:8080
 curl localhost:8080
-#docker rm -f backend && \
-#docker push mrow4a/backend:latest && \
-#docker rm -f frontend && \
-#docker push mrow4a/frontend:latest && \
+
+docker rm -f backend && \
+docker push mrow4a/backend:latest && \
+docker rm -f frontend && \
+docker push mrow4a/frontend:latest && \
 echo "Finished with success"
